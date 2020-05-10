805d7a0037b69505162d9d4cbbe758714b366114
virgil <1195223570@qq.com>
2020-05-10 21:00:14 +0800

    first commit

---
+++ src/main/java/com/virgilin/basics/lambda/LambdsTest.java
@@ -0,0 +1,22 @@
+package com.virgilin.basics.lambda;
+
+/**
+ * Lambda 表达式，也可称为闭包，它是推动 Java 8 发布的最重要新特性。
+ * Lambda 允许把函数作为一个方法的参数（函数作为参数传递进方法中）。
+ * 使用 Lambda 表达式可以使代码变的更加简洁紧凑。
+ * lambda 表达式的语法格式如下：
+ * (parameters) -> expression
+ * 或
+ * (parameters) ->{ statements; }
+ */
+public class LambdsTest {
+    public static void main(String args[]) {
+        final int num = 1;
+        Converter<Integer, String> s = (param) -> System.out.println(String.valueOf(param + num));
+        s.convert(2);  // 输出结果为 3
+    }
+
+    public interface Converter<T1, T2> {
+        void convert(int i);
+    }
+}
